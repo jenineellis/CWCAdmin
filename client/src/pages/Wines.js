570d0b658b95081cc3112075fe42d054940477b1
Matthew Heimark <matthew.heimark@gmail.com>
2019-09-08 15:36:58 -0500

    Empty wines page in react created

---
+++ client/src/pages/Wines.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Wines extends React.Component {
+
+    render() {
+        return (
+            <div>The wines page goes here</div>
+        );
+    }
+
+}
+
+export default Wines;

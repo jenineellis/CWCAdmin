874145eff77631ad8dc32baf2dc4671cdc1af03b
Matthew Heimark <matthew.heimark@gmail.com>
2019-09-18 20:08:46 -0500

    Wines API can now filter by color

--- controllers/winesController.js
+++ controllers/winesController.js
@@ -4,8 +4,14 @@ const db = require("../models");
 module.exports = {
 
   findAll: function(req, res) {
+    var filter = {};
+
+    // req.query contains all the query string parameters;
+    if (req.query.color)
+      filter.color = req.query.color;
+
     db.Wine
-      .find()
+      .find(filter)
       .limit(25)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));

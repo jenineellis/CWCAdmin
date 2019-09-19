30e372d57099ad8db584e1fb86ac04b420cc67ba
Matthew Heimark <matthew.heimark@gmail.com>
2019-09-18 21:01:39 -0500

    Removed filters that created conflicts

--- client/src/pages/Wines.js
+++ client/src/pages/Wines.js
@@ -44,40 +44,22 @@ class Wines extends React.Component {
                         <option value="sparkling">Sparkling</option>
                     </select>
                     <select>
-                        <option selected>Vineyard</option>
-                        <option value="1">Caymus</option>
-                        <option value="2">Silver Oak</option>
-                        <option value="3">Stag's Leap</option>
-                    </select>
-                    <select>
-                        <option selected>Nested Region</option>
+                        <option selected>Region</option>
                         <option value="1">Carneros</option>
                         <option value="2">Howell Mountain</option>
                         <option value="3">Oakville</option>
-                    </select>
-                    <select>
-                        <option selected>Vintage</option>
-                        <option value="1">2018</option>
-                        <option value="2">2017</option>
-                        <option value="3">2016</option>
-                    </select>
-                    <select>
-                        <option selected>Alcohol Percentage</option>
-                        <option value="1">0-9.9%</option>
-                        <option value="2">10-14.9%</option>
-                        <option value="3">15+%</option>
-                    </select>
-                    <select>
-                        <option selected>Price Range</option>
-                        <option value="1">0-$25</option>
-                        <option value="2">$25.01-$49.99</option>
-                        <option value="3">$50+</option>
                     </select>
                     <select>
                         <option selected>Varietal</option>
                         <option value="1">Cabernet Sauvignon</option>
                         <option value="2">Merlot</option>
                         <option value="3">Petit Syrah</option>
+                    </select>
+                    <select>
+                        <option selected>Price Range</option>
+                        <option value="1">0-$25</option>
+                        <option value="2">$25.01-$49.99</option>
+                        <option value="3">$50+</option>
                     </select>
                 </form>
 

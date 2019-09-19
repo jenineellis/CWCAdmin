ca78d8a7d74d6ad2f1a51e63439eca830b14ce0b
Matthew Heimark <matthew.heimark@gmail.com>
2019-09-18 23:33:01 -0500

    Changed Vino Tantum to CWC

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        Vino Tantum
+        CWC
       </a>
 
       <ul className="navbar-nav mr-auto">
